4a287230e9b6c6de0fa56a71dbc9c37b19eeeabc
ullysse123 <jujudu31140@hotmail.fr>
2017-05-16 22:04:05 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,47 @@
 #include "jouer.h"
 
 int main(){
- 
+    //Menu de lancement :
+        //Nouvelle Partie
+        //Charger Partie
+        //Quitter le jeux
+    
+    //Si Nouvelle partie
+        //Lancer une nouvelle parie
+    //Si Charger Partie
+        //Charger la partie
+    //Si quitter
+        //Fermer le programme
+    
+    //Tant que Arret est faux
+        //Jouer
+        //Afficher plateau
+        //Menu de jeux :
+            //Valider coup  
+            //Annuler le coup
+            //Sauvegarder
+            //Abandonner ( demande une si il veut sauvegarder )
+        //Si il veux sauvegarder
+            //Sauvegarder
+        //Si il veux valider
+            //Passe au joueur suivant
+            //Verifier si est fini et recuperer la couleur
+            //Si est fini = NOIR ou ROUGE alors Arret deviens vrai
+        //Sinon si il veux abandonner
+            //Lancer abandonner
+            //Passer arret a vrai
+        //Sinon si il veut annuler
+            //Annuler et laisser reboucler
+    
+    //Si la couleur est différente de vide
+        //Afficher le gagnant et le plateau
+    
+    //Menu de fin:
+        //Nouvelle partie
+        //Charger Partie
+        //Quitter le jeux
+    
+    //En fonction du choix reboucler
+        
     return 0;
 }
